ed988077fd2ae5582d893b5f78bc54c3c1ad1a6a
Ankit Soni <akt_soni@yahoo.co.in>
2022-09-03 01:22:49 +0200

    react reveal added with animation

--- src/components/CardUserSpecific.js
+++ src/components/CardUserSpecific.js
@@ -4,6 +4,7 @@ import { useLocalStorage } from './useLocalStorage';
 import { ToastContainer, toast } from "react-toastify";
 import { FaEnvelope, FaMapMarkerAlt, FaHeart, FaHourglassHalf, FaPhoneVolume, FaBirthdayCake } from "react-icons/fa";
 import moment from "moment";
+import Fade from 'react-reveal/Fade';
 
 function CardUserSpecific({ open, onClose, passedId, user }) {
 
@@ -13,8 +14,7 @@ function CardUserSpecific({ open, onClose, passedId, user }) {
 
     const findExistingUser = itemsInStorage.find(user => user.login.uuid === passedId)
 
-
-    // if (!open) return null;
+    if (!open) return null;
 
     // Add favourite to localtorage and ui
     const addTofavourite = () => {
@@ -53,72 +53,74 @@ function CardUserSpecific({ open, onClose, passedId, user }) {
             <ToastContainer autoClose={2000} />
 
             {/* modal body grid Container*/}
-            <div className='container grid grid-cols-1 md:grid-cols-3 max-w-4xl shadow-xl p-4 bg-white z-10'>
+            <Fade cascade>
+                <div className='container grid grid-cols-1 md:grid-cols-3 max-w-4xl shadow-xl p-4 bg-white z-10'>
 
-                {/* grid item 1  */}
-                <div className="grid grid-rows-2 p-4 items-center ">
-                    <div style={{ background: `url(${user.picture.large}) no-repeat center`, backgroundSize: "cover", width: "100%", height: "100%" }}  ></div>
-                    <div className='grid grid-rows-2 gap-2 mt-6'>
-                        <a href={`mailto:${user.email}`} rel="noreferrer" className="flex items-center gap-2 btn justify-center w-full ">
-                            <FaEnvelope />Send Email</a>
-                        {!findExistingUser && <button className="flex items-center gap-2 btn justify-center w-full " onClick={addTofavourite}><FaHeart />Add to favourite</button>}
-                        {findExistingUser && <button className="btn bg-red-500" onClick={removeFavourite}>Remove Favourite</button>}
+                    {/* grid item 1  */}
+                    <div className="grid grid-rows-2 p-4 items-center ">
+                        <div style={{ background: `url(${user.picture.large}) no-repeat center`, backgroundSize: "cover", width: "100%", height: "100%" }}  ></div>
+                        <div className='grid grid-rows-2 gap-2 mt-6'>
+                            <a href={`mailto:${user.email}`} rel="noreferrer" className="flex items-center gap-2 btn justify-center w-full ">
+                                <FaEnvelope />Send Email</a>
+                            {!findExistingUser && <button className="flex items-center gap-2 btn justify-center w-full " onClick={addTofavourite}><FaHeart />Add to favourite</button>}
+                            {findExistingUser && <button className="btn bg-red-500" onClick={removeFavourite}>Remove Favourite</button>}
 
+                        </div>
                     </div>
-                </div>
-                {/* end of grid item */}
+                    {/* end of grid item */}
 
-                {/* grid item 2 */}
-                <div className='p-6 flex flex-col gap-2 md:col-span-2 relative'>
-                    <h2 >{user.name.title}. {user.name.first} {user.name.last} <span className='text-lg'>Age: {user.dob.age}</span></h2>
-                    <div className='flex-col gap-4 flex'>
-                        <p className='flex items-center gap-4 '>
-                            <FaMapMarkerAlt />{user.location.street.name}, {user.location.street.number}, {user.location.city} {user.location.postcode}
-                        </p>
-                        <p className='flex items-center gap-4 '><FaEnvelope />{user.email}</p>
-                        <div className='grid grid-cols-1 sm:grid-cols-2'>
-                            <div className='flex flex-col shadow-lg p-3 gap-2  items-center  '>
-                                <span>User Id</span>
-                                <p className='text-teal-600 '>{id ? id : setId("Not available")}</p>
-                            </div>
-                            <div className='flex flex-col shadow-lg p-3 gap-2  items-center'>
-                                <span>Date of birth</span>
-                                <p className='text-teal-600 '>{dob}</p>
-                            </div>
-                            <div className='flex flex-col shadow-lg p-3 gap-2  items-center'>
-                                <span>First-registered</span>
-                                <p className='text-teal-600 '>{firstRegistered}</p>
-                            </div>
-                            <div className='flex flex-col shadow-lg p-3 gap-2 items-center'>
-                                <span>Country</span>
-                                <p className='text-teal-600 '>{user.location.country}</p>
+                    {/* grid item 2 */}
+                    <div className='p-6 flex flex-col gap-2 md:col-span-2 relative'>
+                        <h2 >{user.name.title}. {user.name.first} {user.name.last} <span className='text-lg'>Age: {user.dob.age}</span></h2>
+                        <div className='flex-col gap-4 flex'>
+                            <p className='flex items-center gap-4 '>
+                                <FaMapMarkerAlt />{user.location.street.name}, {user.location.street.number}, {user.location.city} {user.location.postcode}
+                            </p>
+                            <p className='flex items-center gap-4 '><FaEnvelope />{user.email}</p>
+                            <div className='grid grid-cols-1 sm:grid-cols-2'>
+                                <div className='flex flex-col shadow-lg p-3 gap-2  items-center  '>
+                                    <span>User Id</span>
+                                    <p className='text-teal-600 '>{id ? id : setId("Not available")}</p>
+                                </div>
+                                <div className='flex flex-col shadow-lg p-3 gap-2  items-center'>
+                                    <span>Date of birth</span>
+                                    <p className='text-teal-600 '>{dob}</p>
+                                </div>
+                                <div className='flex flex-col shadow-lg p-3 gap-2  items-center'>
+                                    <span>First-registered</span>
+                                    <p className='text-teal-600 '>{firstRegistered}</p>
+                                </div>
+                                <div className='flex flex-col shadow-lg p-3 gap-2 items-center'>
+                                    <span>Country</span>
+                                    <p className='text-teal-600 '>{user.location.country}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    {/* end of grid item */}
                 </div>
-                {/* end of grid item */}
-            </div>
-            {/* end og grid Container*/}
-            <div className=' w-full max-w-4xl z-10 '>
-                <div className='grid grid-cols-3 gap-2  text-center text-white'>
-                    <div className='flex flex-col shadow-lg p-3 bg-indigo-800 gap-3 items-center justify-around'>
-                        <FaHourglassHalf className='text-3xl text-teal-600' />
-                        <span>Time Since Registered</span>
-                        <p className='text-teal-300'>{timeSinceRegistered}</p>
-                    </div>
-                    <div className='flex flex-col shadow-lg p-3 bg-indigo-800 gap-3 items-center justify-around '>
-                        <FaBirthdayCake className='text-3xl text-teal-600 ' />
-                        <span>Days until next Birthday</span>
-                        <p className='text-teal-300'>{timeUntilNextBirthday}</p>
+                {/* end og grid Container*/}
+                <div className=' w-full max-w-4xl z-10 '>
+                    <div className='grid grid-cols-3 gap-2  text-center text-white'>
+                        <div className='flex flex-col shadow-lg p-3 bg-indigo-800 gap-3 items-center justify-around'>
+                            <FaHourglassHalf className='text-3xl text-teal-600' />
+                            <span>Time Since Registered</span>
+                            <p className='text-teal-300'>{timeSinceRegistered}</p>
+                        </div>
+                        <div className='flex flex-col shadow-lg p-3 bg-indigo-800 gap-3 items-center justify-around '>
+                            <FaBirthdayCake className='text-3xl text-teal-600 ' />
+                            <span>Days until next Birthday</span>
+                            <p className='text-teal-300'>{timeUntilNextBirthday}</p>
 
-                    </div>
-                    <div className='flex shadow-lg flex-col p-3 bg-indigo-800 gap-3 justify-around items-center '>
-                        <FaPhoneVolume className='text-3xl text-teal-600 ' />
-                        <span>Telephone</span>
-                        <p className='text-teal-300'>{user.cell}</p>
+                        </div>
+                        <div className='flex shadow-lg flex-col p-3 bg-indigo-800 gap-3 justify-around items-center '>
+                            <FaPhoneVolume className='text-3xl text-teal-600 ' />
+                            <span>Telephone</span>
+                            <p className='text-teal-300'>{user.cell}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Fade>
         </section >,
         document.getElementById("portal")
     )
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { FaEnvelope, FaMapMarkerAlt, FaHeart } from "react-icons/fa";
 import { useLocalStorage } from './useLocalStorage';
 import CardUserSpecific from './CardUserSpecific';
+import Flip from 'react-reveal/Flip';
 
 
 
@@ -32,21 +33,23 @@ function UserCard({ user }) {
 
 
     return (
-        <div className=' relative'>
-            <div className="text-slate-900 hover:text-slate-800 card" onClick={openModal} >
-                <figure className=" relative w-full h-full border-b bg-slate-200" >
-                    <img className="object-cover h-36 w-36 rounded-full mx-auto absolute top-14 right-0 left-0 bottom-0 shadow-md" src={user.picture.large} />
-                </figure>
-                <div className='p-5 flex flex-col  row-span-2 gap-1 mt-3'>
-                    <h3 >{user.name.first} {user.name.last}</h3>
-                    <p>Age: {user.dob.age},</p>
-                    <p className='flex items-center gap-2'><FaMapMarkerAlt />{user.location.city}</p>
-                    <p className='flex items-center gap-2'><FaEnvelope />{user.email}</p>
+        <Flip left cascade>
+            <div className=' relative'>
+                <div className="text-slate-900 hover:text-slate-800 card" onClick={openModal} >
+                    <figure className=" relative w-full h-full border-b bg-slate-200" >
+                        <img className="object-cover h-36 w-36 rounded-full mx-auto absolute top-14 right-0 left-0 bottom-0 shadow-md" src={user.picture.large} />
+                    </figure>
+                    <div className='p-5 flex flex-col  row-span-2 gap-1 mt-3'>
+                        <h3 >{user.name.first} {user.name.last}</h3>
+                        <p>Age: {user.dob.age},</p>
+                        <p className='flex items-center gap-2'><FaMapMarkerAlt />{user.location.city}</p>
+                        <p className='flex items-center gap-2'><FaEnvelope />{user.email}</p>
+                    </div>
                 </div>
+                {isOpen && (<CardUserSpecific key={user.login.uuid} onClose={closeModal} passedId={user.login.uuid} user={user} open={isOpen} />)}
+                {favouriteExist && <button className="heart text-red-500 cursor-pointer" onClick={removeFavourite}><FaHeart /></button>}
             </div>
-            {isOpen && (<CardUserSpecific key={user.login.uuid} onClose={closeModal} passedId={user.login.uuid} user={user} open={isOpen} />)}
-            {favouriteExist && <button className="heart text-red-500 cursor-pointer" onClick={removeFavourite}><FaHeart /></button>}
-        </div>
+        </Flip >
 
     )
 }
