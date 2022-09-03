0982f10a38227b1c4016f20b070d2f6331f76b86
Ankit Soni <akt_soni@yahoo.co.in>
2022-09-03 22:33:01 +0200

    about page added

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <footer className='text-center p-6 mx-8 border-t border-teal-500 '>
-            <p className='text-teal-500 '>Copyright @2022 Designed & Developed by AnkSon</p>
+            <p className='text-slate-700 '>Copyright @2022 Designed & Developed by AnkSon</p>
         </footer>
     )
 }
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+import people from "../../assets/peoplePassport.png"
 
 function About() {
     return (
-        <div>About</div>
+        <section className='flex flex-col justify-center items-center gap-2 ' style={{ minHeight: "70vh" }} >
+
+            <div className='container grid grid-cols-1 md:grid-cols-3 max-w-5xl shadow-xl bg-white'>
+
+                {/* grid item 1  */}
+                <div className="grid grid-rows-2 p-4 items-center ">
+                    <h1 className='mb-4'>Om Appen</h1>
+                    <p>Random User Generator appen henter tilfeldige brukere fra api som genererer falske brukere </p>
+                    <p>Ved å trykke på individuell bruker, meir informasjon blir vist fram om bruker og Appen gir  mulighet til lagre/sette brukere som favourite også. </p>
+                    <p>Appen er utvilet med Reactjs og tailwindCss for å style. React-reveal pakken for animasjoner or React-toastify for toast meldingen.</p>
+                </div>
+                {/* end of grid item */}
+
+                {/* grid item 2 */}
+
+                <figure className='col-span-2  '>
+                    <img src={people} alt="random people" className='about-img' />
+                </figure>
+
+                {/* end of grid item */}
+            </div>
+
+
+        </section >
     )
 }
 
