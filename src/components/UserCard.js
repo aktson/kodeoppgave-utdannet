0ec516426ad1b4c3ece8976de38d800b446e54da
Ankit Soni <akt_soni@yahoo.co.in>
2022-09-04 10:41:57 +0200

    overflow-x updated usercard figure

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -40,7 +40,7 @@ function UserCard({ user }) {
     return (
         <Zoom>
             <div className=' relative'>
-                <div className="text-slate-900 hover:text-slate-800 card overflow-hidden" onClick={openModal} >
+                <div className="text-slate-900 hover:text-slate-800 card overflow-x-hidden" onClick={openModal} >
                     <figure className=" relative w-full h-full border-b bg-slate-200 " >
                         <img className="object-cover h-36 w-36 rounded-full mx-auto absolute top-20 right-0 left-0 shadow-md" src={user.picture.large} />
                     </figure>
