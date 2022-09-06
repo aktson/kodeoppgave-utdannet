404d45ce8490c65824a48d5f5331359243a9e10c
Ankit Soni <akt_soni@yahoo.co.in>
2022-09-06 11:01:21 +0200

    Update About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -12,8 +12,8 @@ function About() {
                     <div className="grid grid-rows-2 p-4 items-center ">
                         <h1 className='mb-4'>Om Appen</h1>
                         <p>Random User Generator appen henter tilfeldige brukere fra api som genererer falske brukere </p>
-                        <p>Ved å trykke på individuell bruker, meir informasjon blir vist fram om bruker og Appen gir  mulighet til lagre/sette brukere som favourite også. </p>
-                        <p>Appen er utvilet med Reactjs og tailwindCss for å style. React-reveal pakken for animasjoner or React-toastify for toast meldingen.</p>
+                        <p>Ved å trykke på individuell bruker, meir informasjon blir vist fram om bruker og appen gir  mulighet til lagre/sette brukere som favourite også. </p>
+                        <p>Appen er utviklet med Reactjs og tailwindCss for å style. React-reveal pakken for animasjoner or React-toastify for toast meldingen.</p>
                     </div>
                     {/* end of grid item */}
 
